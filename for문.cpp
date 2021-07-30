f62f703bd41ea77b87814f83be9c57d576a3c1d3
easyminnn <87521781+easyminnn@users.noreply.github.com>
2021-07-31 02:16:06 +0900

    Add files via upload

---
+++ for문.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main()
+{
+    int sum = 0 ;
+
+    for (int i=0; i<10; i++)
+    {
+        sum += i;
+
+    }
+
+    std::cout << " the sum is... : " << sum << std::endl;
+
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "HI, I'm" << std::endl;
+    std::cout << "Jimin ";
+    std::cout << "My name is " << "Psi" << std::endl;
+    
+    return 0;
+    
+}
